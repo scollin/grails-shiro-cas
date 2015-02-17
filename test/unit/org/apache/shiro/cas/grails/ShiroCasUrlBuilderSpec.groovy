6ff9ace8eacf583b4eb5489f08a6bd919fe9cabc
David M. Carr <dcarr@commercehub.com>
2015-02-17 15:37:48 -0500

    Fix a test

--- test/unit/org/apache/shiro/cas/grails/ShiroCasUrlBuilderSpec.groovy
+++ test/unit/org/apache/shiro/cas/grails/ShiroCasUrlBuilderSpec.groovy
@@ -11,16 +11,16 @@ class ShiroCasUrlBuilderSpec extends Specification {
 
     def "can build from login"() {
         expect:
-        ShiroCasUrlBuilder.forLogin().url == "https://cas.example.com/cas/login?service=https://localhost:8080/app/shiro-cas"
-        ShiroCasUrlBuilder.forLogin().withGateway().url == "https://cas.example.com/cas/login?service=https://localhost:8080/app/shiro-cas&gateway=true"
-        ShiroCasUrlBuilder.forLogin().withRenew().url == "https://cas.example.com/cas/login?service=https://localhost:8080/app/shiro-cas&renew=true"
-        ShiroCasUrlBuilder.forLogin().withQueryParam("token", "12345").url == "https://cas.example.com/cas/login?service=https://localhost:8080/app/shiro-cas&token=12345"
+        ShiroCasUrlBuilder.forLogin().url == "https://cas.example.com/cas/login?service=https://static.example.com/app/shiro-cas"
+        ShiroCasUrlBuilder.forLogin().withGateway().url == "https://cas.example.com/cas/login?service=https://static.example.com/app/shiro-cas&gateway=true"
+        ShiroCasUrlBuilder.forLogin().withRenew().url == "https://cas.example.com/cas/login?service=https://static.example.com/app/shiro-cas&renew=true"
+        ShiroCasUrlBuilder.forLogin().withQueryParam("token", "12345").url == "https://cas.example.com/cas/login?service=https://static.example.com/app/shiro-cas&token=12345"
     }
 
     def "can build from logout"() {
         expect:
-        ShiroCasUrlBuilder.forLogout().url == "https://cas.example.com/cas/logout?service=https://localhost:8080/app/shiro-cas"
-        ShiroCasUrlBuilder.forLogout().withQueryParam("token", "12345").url == "https://cas.example.com/cas/logout?service=https://localhost:8080/app/shiro-cas&token=12345"
+        ShiroCasUrlBuilder.forLogout().url == "https://cas.example.com/cas/logout?service=https://static.example.com/app/shiro-cas"
+        ShiroCasUrlBuilder.forLogout().withQueryParam("token", "12345").url == "https://cas.example.com/cas/logout?service=https://static.example.com/app/shiro-cas&token=12345"
     }
 
     def "can redirect"() {
@@ -30,17 +30,17 @@ class ShiroCasUrlBuilderSpec extends Specification {
         ShiroCasUrlBuilder.forLogin().go(response)
 
         then:
-        1 * response.sendRedirect("https://cas.example.com/cas/login?service=https://localhost:8080/app/shiro-cas")
+        1 * response.sendRedirect("https://cas.example.com/cas/login?service=https://static.example.com/app/shiro-cas")
 
         when:
         ShiroCasUrlBuilder.forLogin().withRenew().withQueryParam("token", "12345").go(response)
 
         then:
-        1 * response.sendRedirect("https://cas.example.com/cas/login?service=https://localhost:8080/app/shiro-cas&renew=true&token=12345")
+        1 * response.sendRedirect("https://cas.example.com/cas/login?service=https://static.example.com/app/shiro-cas&renew=true&token=12345")
     }
 
     def "query parameters are encoded"() {
         expect:
-        ShiroCasUrlBuilder.forLogin().withQueryParam("message", "Welcome to CAS").url == "https://cas.example.com/cas/login?service=https://localhost:8080/app/shiro-cas&message=Welcome%20to%20CAS"
+        ShiroCasUrlBuilder.forLogin().withQueryParam("message", "Welcome to CAS").url == "https://cas.example.com/cas/login?service=https://static.example.com/app/shiro-cas&message=Welcome%20to%20CAS"
     }
 }
